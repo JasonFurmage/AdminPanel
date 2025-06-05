0d59951e5ff96d4c6e9c6ec71a3ed59be7de7d30
JasonFurmage <150832588+JasonFurmage@users.noreply.github.com>
2025-06-05 22:09:10 +0100

    feat: disable registration

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Auth::routes(['register' => false]); // Disable registration.
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
