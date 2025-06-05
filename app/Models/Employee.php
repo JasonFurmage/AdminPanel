cf2e2e500053d07a764167acf6d3ff618efdd4f3
JasonFurmage <150832588+JasonFurmage@users.noreply.github.com>
2025-06-06 00:16:10 +0100

    docs: add comments to employee model

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -9,17 +9,11 @@ class Employee extends Model
 {
     use HasFactory;
 
-    protected $fillable = [
-    'first_name',
-    'last_name',
-    'email',
-    'phone',
-    'company_id',
-];
+    protected $fillable = ['first_name','last_name','email','phone', 'company_id']; // Allow mass-assigning values to these fields.
 
-public function company()
-{
-    return $this->belongsTo(Company::class);
-}
-
+    // Define 'belongs to' relationship with company.
+    public function company()
+    {
+        return $this->belongsTo(Company::class);
+    }
 }
